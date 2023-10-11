16d10ad9989e86750d5cf2c6dee6af08c785e889
mboudet <mateo.boudet@gmail.com>
2023-10-11 17:02:16 +0000

    add proper redirect to gene with annotation

--- imports/ui/genetable/columns/GeneLink.jsx
+++ imports/ui/genetable/columns/GeneLink.jsx
@@ -3,9 +3,15 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function GeneLink({ geneId }) {
+export default function GeneLink({ gene }) {
+  const geneId = gene.ID
+ 
+  const query = new URLSearchParams();
+  query.set("annotation", gene.annotationName);
+  const url = `/gene/${geneId}?${query.toString()}`
+
   return (
-    <Link to={`/gene/${geneId}`} className="genelink" title={geneId}>
+    <Link to={url} className="genelink" title={geneId}>
       { geneId }
     </Link>
   );
